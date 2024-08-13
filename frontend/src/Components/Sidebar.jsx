db09efb9e4b7d3d0cb8358c92e4815d5f0c2fa46
nurulalamador <nurulalomador@gmail.com>
2024-08-14 00:36:33 +0600

    sidebar

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -9,30 +9,34 @@ import 'react-material-symbols/rounded';
 export default function Sidebar({logoutAction, user}) {
     return (
     <div className="sideMenu">
-        <div className="logo">
-            <img src={logo} />
-        </div>
-        <div className="optionContainer">
-            <SidebarOption name="Dashboard" href="/" icon="dashboard"/>
-            <SidebarOption name="Showcase" href="/showcase" icon="gallery_thumbnail"/>
-            <SidebarOption name="Contest" href="/contest" icon="rewarded_ads"/>
-            <SidebarOption name="Community" href="/community" icon="groups"/>
-            <SidebarOption name="Course" href="/course" icon="auto_stories"/>
-            <SidebarOption name="Webinar" href="/webinar" icon="patient_list"/>
-            <SidebarOption name="Marketplace" href="/marketplace" icon="shopping_cart"/>
-            <SidebarOption name="Hiring" href="/hiring" icon="person_search"/>
-            <SidebarOption name="Notifcation" href="/notification" icon="notifications_active"/>
-            <SidebarOption name="Profile" href="/profile" icon="person"/>
-        </div>
-        <div className="profile">
-            <div className="profilePicture">
-                <img src={dp}/>
+        <div className="content">
+            <div className="logo">
+                <img src={logo} />
             </div>
-            <div className="details">
-                <div className="name">{user.name}</div>
-                <div onClick={logoutAction} className="logout">
-                    <MaterialSymbol className="icon" size={20} icon="logout"/>
-                    <div className="text">Logout Account</div>
+            <div className="dynamicGap"></div>
+            <div className="optionContainer">
+                <SidebarOption name="Dashboard" href="/" icon="dashboard"/>
+                <SidebarOption name="Showcase" href="/showcase" icon="gallery_thumbnail"/>
+                <SidebarOption name="Contest" href="/contest" icon="rewarded_ads"/>
+                <SidebarOption name="Community" href="/community" icon="groups"/>
+                <SidebarOption name="Course" href="/course" icon="auto_stories"/>
+                <SidebarOption name="Webinar" href="/webinar" icon="patient_list"/>
+                <SidebarOption name="Marketplace" href="/marketplace" icon="shopping_cart"/>
+                <SidebarOption name="Hiring" href="/hiring" icon="person_search"/>
+                <SidebarOption name="Notifcation" href="/notification" icon="notifications_active"/>
+                <SidebarOption name="Profile" href="/profile" icon="person"/>
+            </div>
+            <div className="dynamicGap"></div>
+            <div className="profile">
+                <div className="profilePicture">
+                    <img src={dp}/>
+                </div>
+                <div className="details">
+                    <div className="name">{user.name}</div>
+                    <div onClick={logoutAction} className="logout">
+                        <MaterialSymbol className="icon" size={20} icon="logout"/>
+                        <div className="text">Logout</div>
+                    </div>
                 </div>
             </div>
         </div>
