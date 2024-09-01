db2a0e3b84a7d8448cdb8774eb3f4fc14f663f47
nurulalamador <nurulalomador@gmail.com>
2024-09-01 19:25:45 +0600

    material icon added

--- frontend/src/Components/StudentComponents/Webinar/WebinarBox.jsx
+++ frontend/src/Components/StudentComponents/Webinar/WebinarBox.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { Clock, Users, CalendarDays } from 'lucide-react';
+import { MaterialSymbol } from "react-material-symbols";
+import "react-material-symbols/rounded";
 
 export default function WebinarBox({
   webinar,
@@ -18,15 +18,15 @@ export default function WebinarBox({
         
         <div className="bg-gray-100 rounded-lg p-4 space-y-2">
           <div className="flex items-center text-gray-700">
-            <CalendarDays className="w-5 h-5 mr-2" />
+            <MaterialSymbol className="w-5 h-5 mr-2" size={24} icon="calendar_month" />
             <span>{startTime.toLocaleDateString()}</span>
           </div>
           <div className="flex items-center text-gray-700">
-            <Clock className="w-5 h-5 mr-2" />
+            <MaterialSymbol className="w-5 h-5 mr-2" size={24} icon="schedule" />
             <span>{startTime.toLocaleTimeString()} - {endTime.toLocaleTimeString()}</span>
           </div>
           <div className="flex items-center text-gray-700">
-            <Users className="w-5 h-5 mr-2" />
+            <MaterialSymbol className="w-5 h-5 mr-2" size={24} icon="person" />
             <span>{webinar.participants_count || 0} participants</span>
           </div>
         </div>
@@ -46,16 +46,3 @@ export default function WebinarBox({
     </div>
   );
 }
-
-WebinarBox.propTypes = {
-  webinar: PropTypes.shape({
-    webinar_id: PropTypes.string.isRequired,
-    webinar_name: PropTypes.string.isRequired,
-    webinar_description: PropTypes.string.isRequired,
-    start_time: PropTypes.string.isRequired,
-    end_time: PropTypes.string.isRequired,
-    participants_count: PropTypes.number,
-  }).isRequired,
-  handleJoinClick: PropTypes.func.isRequired,
-  registrationStatus: PropTypes.bool.isRequired,
-};
